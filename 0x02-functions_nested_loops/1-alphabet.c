7191a796a1505d881fc3a05d486ce27740465164
KirukiNjoki <njokikiruki@gmail.com>
2022-06-08 01:09:39 -0700

    function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include "main.h"
+void main ()
+{
+	int ch;
+	clrscr();
+	_putchar(“Print all lowercase  letters followed by a new line :\n”);
+	/* print lowercase letters */
+	  for (ch= ‘a’; ch<= ‘z’; ch++)
+		_putchar(ch);
+	  _putchar (‘\n’) ;
+	    getch();
+}
+return = 0;
+
