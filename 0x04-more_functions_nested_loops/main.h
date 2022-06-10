4e12a8187d2e38f57a8dfd2ca53e5aba23c49461
KirukiNjoki <njokikiruki@gmail.com>
2022-06-09 18:25:13 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,6 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-
+void print_number(int n);
+int _putchar(char c);
+void print_triangle(int size);
